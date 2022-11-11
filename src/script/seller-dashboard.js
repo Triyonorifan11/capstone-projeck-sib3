ee02c7afc4241d66cde4fede1435ddc01f342104
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-11 19:41:53 +0700

    add loader in dahboard seller

--- src/script/seller-dashboard.js
+++ src/script/seller-dashboard.js
@@ -1,5 +1,8 @@
 import 'regenerator-runtime';
+import '../styles/homepage.css';
 import App from './views/seller/app';
+
+const loader = document.querySelector('#js-preloader');
 
 const app = new App({
   header: document.querySelector('#header'),
@@ -9,8 +12,10 @@ const app = new App({
 
 window.addEventListener('hashchange', () => {
   app.renderPage();
+  loader.classList.add('loaded');
 });
 
 window.addEventListener('load', () => {
+  loader.classList.add('loaded');
   app.renderPage();
 });
