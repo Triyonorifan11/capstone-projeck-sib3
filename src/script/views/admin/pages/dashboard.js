267c110bad6de676f9b3ae7d2a34a9b5fecd2b28
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-11 19:24:42 +0700

    update library datatable in admin page, and navlink when click then active

--- src/script/views/admin/pages/dashboard.js
+++ src/script/views/admin/pages/dashboard.js
@@ -178,7 +178,7 @@ const DashboardAdmin = {
                 <div class="card-body">
                   <h5 class="card-title">Recent Sales <span>| Today</span></h5>
 
-                  <table class="table table-borderless datatable">
+                  <table class="table table-borderless" id="adminDatatable">
                     <thead>
                       <tr>
                         <th scope="col">#</th>
@@ -258,7 +258,7 @@ const DashboardAdmin = {
                 <div class="card-body pb-0">
                   <h5 class="card-title">Top Selling <span>| Today</span></h5>
 
-                  <table class="table table-borderless">
+                  <table class="table table-borderless" id="adminDatatable">
                     <thead>
                       <tr>
                         <th scope="col">Preview</th>
@@ -330,61 +330,6 @@ const DashboardAdmin = {
 
               <div id="trafficChart" style="min-height: 400px;" class="echart"></div>
 
-              <script>
-                document.addEventListener("DOMContentLoaded", () => {
-                  echarts.init(document.querySelector("#trafficChart")).setOption({
-                    tooltip: {
-                      trigger: 'item'
-                    },
-                    legend: {
-                      top: '5%',
-                      left: 'center'
-                    },
-                    series: [{
-                      name: 'Access From',
-                      type: 'pie',
-                      radius: ['40%', '70%'],
-                      avoidLabelOverlap: false,
-                      label: {
-                        show: false,
-                        position: 'center'
-                      },
-                      emphasis: {
-                        label: {
-                          show: true,
-                          fontSize: '18',
-                          fontWeight: 'bold'
-                        }
-                      },
-                      labelLine: {
-                        show: false
-                      },
-                      data: [{
-                        value: 1048,
-                        name: 'Search Engine'
-                      },
-                      {
-                        value: 735,
-                        name: 'Direct'
-                      },
-                      {
-                        value: 580,
-                        name: 'Email'
-                      },
-                      {
-                        value: 484,
-                        name: 'Union Ads'
-                      },
-                      {
-                        value: 300,
-                        name: 'Video Ads'
-                      }
-                      ]
-                    }]
-                  });
-                });
-              </script>
-
             </div>
           </div><!-- End Website Traffic -->
         </div><!-- End Right side columns -->
@@ -395,55 +340,105 @@ const DashboardAdmin = {
   },
 
   async afterRender() {
-    document.addEventListener('DOMContentLoaded', () => {
-      new ApexCharts(document.querySelector('#reportsChart'), {
-        series: [{
-          name: 'Sales',
-          data: [31, 40, 28, 51, 42, 82, 56],
-        }, {
-          name: 'Revenue',
-          data: [11, 32, 45, 32, 34, 52, 41],
-        }, {
-          name: 'Customers',
-          data: [15, 11, 32, 18, 9, 24, 11],
-        }],
-        chart: {
-          height: 350,
-          type: 'area',
-          toolbar: {
-            show: false,
+    $('#adminDatatable').DataTable();
+    new ApexCharts(document.querySelector('#reportsChart'), {
+      series: [{
+        name: 'Sales',
+        data: [31, 40, 28, 51, 42, 82, 56],
+      }, {
+        name: 'Revenue',
+        data: [11, 32, 45, 32, 34, 52, 41],
+      }, {
+        name: 'Customers',
+        data: [15, 11, 32, 18, 9, 24, 11],
+      }],
+      chart: {
+        height: 350,
+        type: 'area',
+        toolbar: {
+          show: false,
+        },
+      },
+      markers: {
+        size: 4,
+      },
+      colors: ['#4154f1', '#2eca6a', '#ff771d'],
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.3,
+          opacityTo: 0.4,
+          stops: [0, 90, 100],
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: 'smooth',
+        width: 2,
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: ['2018-09-19T00:00:00.000Z', '2018-09-19T01:30:00.000Z', '2018-09-19T02:30:00.000Z', '2018-09-19T03:30:00.000Z', '2018-09-19T04:30:00.000Z', '2018-09-19T05:30:00.000Z', '2018-09-19T06:30:00.000Z'],
+      },
+      tooltip: {
+        x: {
+          format: 'dd/MM/yy HH:mm',
+        },
+      },
+    }).render();
+
+    echarts.init(document.querySelector('#trafficChart')).setOption({
+      tooltip: {
+        trigger: 'item',
+      },
+      legend: {
+        top: '5%',
+        left: 'center',
+      },
+      series: [{
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center',
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: '18',
+            fontWeight: 'bold',
           },
         },
-        markers: {
-          size: 4,
+        labelLine: {
+          show: false,
         },
-        colors: ['#4154f1', '#2eca6a', '#ff771d'],
-        fill: {
-          type: 'gradient',
-          gradient: {
-            shadeIntensity: 1,
-            opacityFrom: 0.3,
-            opacityTo: 0.4,
-            stops: [0, 90, 100],
-          },
+        data: [{
+          value: 1048,
+          name: 'Search Engine',
         },
-        dataLabels: {
-          enabled: false,
+        {
+          value: 735,
+          name: 'Direct',
         },
-        stroke: {
-          curve: 'smooth',
-          width: 2,
+        {
+          value: 580,
+          name: 'Email',
         },
-        xaxis: {
-          type: 'datetime',
-          categories: ['2018-09-19T00:00:00.000Z', '2018-09-19T01:30:00.000Z', '2018-09-19T02:30:00.000Z', '2018-09-19T03:30:00.000Z', '2018-09-19T04:30:00.000Z', '2018-09-19T05:30:00.000Z', '2018-09-19T06:30:00.000Z'],
+        {
+          value: 484,
+          name: 'Union Ads',
         },
-        tooltip: {
-          x: {
-            format: 'dd/MM/yy HH:mm',
-          },
+        {
+          value: 300,
+          name: 'Video Ads',
         },
-      }).render();
+        ],
+      }],
     });
   },
 };
