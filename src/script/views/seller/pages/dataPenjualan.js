a64766361f492af77c7825cb0ce99ee74e45a025
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-11 15:22:58 +0700

    add button data penjualan

--- src/script/views/seller/pages/dataPenjualan.js
+++ src/script/views/seller/pages/dataPenjualan.js
@@ -20,24 +20,14 @@ const DataPenjualan = {
             <div class="row">
               <!-- Recent Sales -->
               <div class="col-12">
+              
                 <div class="card recent-sales overflow-auto">
-
-                  <div class="filter">
-                    <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                      <li class="dropdown-header text-start">
-                        <h6>Filter</h6>
-                      </li>
-
-                      <li><a class="dropdown-item" href="#">Today</a></li>
-                      <li><a class="dropdown-item" href="#">This Month</a></li>
-                      <li><a class="dropdown-item" href="#">This Year</a></li>
-                    </ul>
-                  </div>
-
                   <div class="card-body">
-                    <h5 class="card-title">Recent Sales</h5>
-
+                    <h5 class="card-title">
+                    Recent Sales
+                    <a href="#tambahDataPenjualan" class="btn btn-primary ms-4"><i class="bi bi-plus-circle"></i> Tambah Data</a>
+                    </h5>
+                    
                     <table class="table table-borderless" id="dataseller2">
                       <thead>
                         <tr>
@@ -46,6 +36,7 @@ const DataPenjualan = {
                           <th scope="col">Product</th>
                           <th scope="col">Price</th>
                           <th scope="col">Status</th>
+                          <th scope="col">Actions</th>
                         </tr>
                       </thead>
                       <tbody>
@@ -55,6 +46,10 @@ const DataPenjualan = {
                           <td><a href="#" class="text-primary">At praesentium minu</a></td>
                           <td>$64</td>
                           <td><span class="badge bg-success">Approved</span></td>
+                          <td>
+                            <a href="#" class="btn btn-sm btn-secondary rounded-circle"><i class="bi bi-pencil-square"></i></a>
+                            <a href="#" class="btn btn-sm btn-danger rounded-circle"><i class="bi bi-trash"></i></a>
+                          </td>
                         </tr>
                         <tr>
                           <th scope="row"><a href="#">#2147</a></th>
@@ -62,6 +57,10 @@ const DataPenjualan = {
                           <td><a href="#" class="text-primary">Blanditiis dolor omnis similique</a></td>
                           <td>$47</td>
                           <td><span class="badge bg-warning">Pending</span></td>
+                          <td>
+                            <a href="#" class="btn btn-sm btn-secondary rounded-circle"><i class="bi bi-pencil-square"></i></a>
+                            <a href="#" class="btn btn-sm btn-danger rounded-circle"><i class="bi bi-trash"></i></a>
+                          </td>
                         </tr>
                         <tr>
                           <th scope="row"><a href="#">#2049</a></th>
@@ -69,6 +68,10 @@ const DataPenjualan = {
                           <td><a href="#" class="text-primary">At recusandae consectetur</a></td>
                           <td>$147</td>
                           <td><span class="badge bg-success">Approved</span></td>
+                          <td>
+                            <a href="#" class="btn btn-sm btn-secondary rounded-circle"><i class="bi bi-pencil-square"></i></a>
+                            <a href="#" class="btn btn-sm btn-danger rounded-circle"><i class="bi bi-trash"></i></a>
+                          </td>
                         </tr>
                         <tr>
                           <th scope="row"><a href="#">#2644</a></th>
@@ -76,6 +79,10 @@ const DataPenjualan = {
                           <td><a href="#" class="text-primar">Ut voluptatem id earum et</a></td>
                           <td>$67</td>
                           <td><span class="badge bg-danger">Rejected</span></td>
+                          <td>
+                            <a href="#" class="btn btn-sm btn-secondary rounded-circle"><i class="bi bi-pencil-square"></i></a>
+                            <a href="#" class="btn btn-sm btn-danger rounded-circle"><i class="bi bi-trash"></i></a>
+                          </td>
                         </tr>
                         <tr>
                           <th scope="row"><a href="#">#2644</a></th>
@@ -83,6 +90,10 @@ const DataPenjualan = {
                           <td><a href="#" class="text-primary">Sunt similique distinctio</a></td>
                           <td>$165</td>
                           <td><span class="badge bg-success">Approved</span></td>
+                          <td>
+                            <a href="#" class="btn btn-sm btn-secondary rounded-circle"><i class="bi bi-pencil-square"></i></a>
+                            <a href="#" class="btn btn-sm btn-danger rounded-circle"><i class="bi bi-trash"></i></a>
+                          </td>
                         </tr>
                       </tbody>
                     </table>
