96cd9f07968bacb57e0c4b79dfac2c86f68ef6d3
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-13 11:19:26 +0700

    add firebase config and install firebase node module

---
+++ src/script/global/firebase-config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// import { getDatabase, ref, set } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDCKibznJSv0-KZwX2iNKl_Xm2ZAMnuNLY',
+  authDomain: 'capstone-project-sib3.firebaseapp.com',
+  databaseURL: 'https://capstone-project-sib3-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'capstone-project-sib3',
+  storageBucket: 'capstone-project-sib3.appspot.com',
+  messagingSenderId: '1034803075706',
+  appId: '1:1034803075706:web:692fce32acc7a94ee2f9c3',
+  measurementId: 'G-KTWTXBRJ6N',
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
+// const analytics = getAnalytics(app);
