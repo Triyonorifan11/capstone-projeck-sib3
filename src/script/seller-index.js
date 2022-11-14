08fe209b1422a61f19b3d5910d6f0855409eeb6a
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-14 15:07:13 +0700

    update for akses page seller for user seller

--- src/script/seller-index.js
+++ src/script/seller-index.js
@@ -5,7 +5,13 @@ import '../styles/about-blog.css';
 console.log('seller index');
 
 function isLoginSeller() {
-  if (!localStorage.getItem('user_seller')) {
+  const userRT = localStorage.getItem('user_RTproject');
+  const data = JSON.parse(userRT);
+  if (!userRT) {
+    window.location.href = '../login.html';
+  }
+
+  if (data.user !== 'seller') {
     window.location.href = '../login.html';
   }
 }
