7953e0eecd8448cc7829e3ff3605e8aad98db616
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-15 12:44:29 +0700

    update preference file first access

--- src/script/index.js
+++ src/script/index.js
@@ -1,13 +1,26 @@
 import 'regenerator-runtime';
 import '../styles/homepage.css';
+import '../styles/about-blog.css';
 
-console.log('this for preference');
+console.log('seller index');
+
+function isLoginSeller() {
+  const userRT = localStorage.getItem('user_RTproject');
+  const data = JSON.parse(userRT);
+  if (!userRT) {
+    window.location.href = '../login.html';
+  }
+
+  if (data.user !== 'seller') {
+    window.location.href = '../login.html';
+  }
+}
+
 const loader = document.querySelector('#js-preloader');
-
 window.addEventListener('load', () => {
-  console.log('loading..');
+  isLoginSeller();
+  loader.classList.remove('loaded');
   setTimeout(() => {
     loader.classList.add('loaded');
-    console.log('finish');
-  }, 2400);
+  }, 2000);
 });
