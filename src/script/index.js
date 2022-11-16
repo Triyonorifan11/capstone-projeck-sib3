cb5659549765c3f9d782573abf8797e4edaf0894
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-16 10:27:32 +0700

    off islogin function

--- src/script/index.js
+++ src/script/index.js
@@ -4,21 +4,21 @@ import '../styles/about-blog.css';
 
 console.log('seller index');
 
-function isLoginSeller() {
-  const userRT = localStorage.getItem('user_RTproject');
-  const data = JSON.parse(userRT);
-  if (!userRT) {
-    window.location.href = '../login.html';
-  }
+// function isLoginSeller() {
+//   const userRT = localStorage.getItem('user_RTproject');
+//   const data = JSON.parse(userRT);
+//   if (!userRT) {
+//     window.location.href = '../login.html';
+//   }
 
-  if (data.user !== 'seller') {
-    window.location.href = '../login.html';
-  }
-}
+//   if (data.user !== 'seller') {
+//     window.location.href = '../login.html';
+//   }
+// }
 
 const loader = document.querySelector('#js-preloader');
 window.addEventListener('load', () => {
-  isLoginSeller();
+  // isLoginSeller();
   loader.classList.remove('loaded');
   setTimeout(() => {
     loader.classList.add('loaded');
