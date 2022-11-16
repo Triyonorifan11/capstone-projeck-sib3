faf2dbd6d4e89a796d5601263809d774ccd64e19
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-16 12:46:40 +0700

    merger conflict and add function in btn dashboard before login

--- src/script/index.js
+++ src/script/index.js
@@ -4,21 +4,21 @@ import '../styles/about-blog.css';
 
 console.log('seller index');
 
-// function isLoginSeller() {
-//   const userRT = localStorage.getItem('user_RTproject');
-//   const data = JSON.parse(userRT);
-//   if (!userRT) {
-//     window.location.href = '../login.html';
-//   }
-
-//   if (data.user !== 'seller') {
-//     window.location.href = '../login.html';
-//   }
-// }
+const dashboardlink = document.getElementById('dashboardlink');
+function isLogin() {
+  const userRT = localStorage.getItem('user_RTproject');
+  const data = JSON.parse(userRT);
+  if (!userRT) {
+    dashboardlink.innerHTML = 'Login';
+    dashboardlink.getAttribute('href');
+  } else if ((data.user !== 'seller') || (data.user !== 'admin') || data.user !== 'buyer') {
+    dashboardlink.innerHTML = 'Login';
+  }
+}
 
 const loader = document.querySelector('#js-preloader');
 window.addEventListener('load', () => {
-  // isLoginSeller();
+  isLogin();
   loader.classList.remove('loaded');
   setTimeout(() => {
     loader.classList.add('loaded');
