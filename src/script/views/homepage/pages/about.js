f0710a5e3c738c21025c711ebf9393245a1b74c7
Hesti Yuana Mahardika <hestiyuanamahardika@gmail.com>
2022-11-26 07:12:39 +0700

    Update About Us Detail

--- src/script/views/homepage/pages/about.js
+++ src/script/views/homepage/pages/about.js
@@ -2,41 +2,56 @@ const About = {
 
   async render() {
     return `
-    <main className='about-wrapper'>
-      <div className='container'>
-        <div className='about-content-wrapper'>
-          <div className='row responsive-about hero-about d-flex pt-3 pb-3 px-md-0'>
-            <div className='col-6 m-auto d-flex'>
-              <img src="../../assets/img/logo500.png" alt="Logo Project" class="rounded mx-auto d-block" width='25%' /><br><br>
-            </div>
-            <div className='col-6 about-us-head'>
-              <h1 class="fw-bold text-center" style="color: #012970;">Tentang Reforma Trash</h1>
-              <p className='desc-highlight' align="justify">Reforma Trash merupakan sistem yang dapat memberikan informasi kepada masyarakat untuk mencari informasi terkait sampah di Indonesia. Penggunaan sistem berbasis website ini diharapkan dapat memudahkan masyarakat untuk melakukan jual beli sampah daur ulang secara online. Selain itu dapat membantu pengepul sampah di Indonesia untuk mengumpulkan sampah. Website tersebut memiliki beberapa fitur antara lain jenis-jenis sampah daur ulang, berita sampah di Indonesia, bahkan memiliki sistem monitoring penjualan sampah seperti barang, rekap penjualan, dan akun penjual serta pembeli.</p>
-            </div>
-          </div>
+    <div class="container-fluid body">
+    <div class="row container-fluid" id="section1">
+      <div class="col-sm-4 col-md-4 col-lg-4">
+        <img src="../../assets/img/logo500.png" alt="Ada Lovelace Portrait" class="img-responsive center-block img-circle" id="adaPortrait">
+        </a>
+      </div>
+      
+      <div class="col-sm-8 col-md-8 col-lg-8 textbox">
+        <p class="text-center" id="title">Tentang Reforma Trash</p>
+        <br>
+        <p class="text-justify text-center" id="bio">
+        Reforma Trash merupakan sistem yang dapat memberikan informasi kepada masyarakat untuk mencari informasi terkait sampah di Indonesia. Penggunaan sistem berbasis website ini diharapkan dapat memudahkan masyarakat untuk melakukan jual beli sampah daur ulang secara online. Selain itu dapat membantu pengepul sampah di Indonesia untuk mengumpulkan sampah. Website tersebut memiliki beberapa fitur antara lain jenis-jenis sampah daur ulang, berita sampah di Indonesia, bahkan memiliki sistem monitoring penjualan sampah seperti barang, rekap penjualan, dan akun penjual serta pembeli.
+        </p>
+      </div>
+    </div>
+    </div>
 
-          <div className='row responsive-about hero-about d-flex pt-3 pb-3 px-md-0'>
-            <div className='col-6'>
-              <img src="../../assets/img/about-us.png" alt="About Us Illustrator" class="rounded mx-auto d-block" width='50%' />
-            </div>
-            <div className='col-6 why-use-head'>
-              <h1 class="fw-bold text-center" style="color: #012970;">Alasan Penggunaan</h1>
-              <p className='desc-highlight' align="justify">
-              Reforma Trash dapat memberikan akses kepada administrator untuk dapat memasukan data penjual, pembeli, blog, dan rekap penjualan sehingga setiap data yang diinputkan dapat menjadikan bahan evaluasi bahkan untuk blog akan ditampilkan kepada pengguna. Website ini dapat berkembang melalui dukungan masyarakat. Sehingga diharapkan dapat memberikan kesadaran masyarakat bahwa sampah memiliki nilai serta menjaga lingkungan tetap bersih. Selain itu diharapkan dapat meningkatkan produktivitas pengepul sampah sehingga efisisensi tenaga dan waktu dalam mengumpulkan sampah.</p><br><br><br>
-            </div>
-          </div>
+    <div class="container-fluid body">
+    <div class="row container-fluid" id="section1">
+      <div class="col-sm-8 col-md-8 col-lg-8 textbox">
+        <p class="text-center" id="title">Alasan Penggunaan</p>
+        <br>
+        <p class="text-justify text-center" id="bio">
+        Reforma Trash dapat memberikan akses kepada administrator untuk dapat memasukan data penjual, pembeli, blog, dan rekap penjualan sehingga setiap data yang diinputkan dapat menjadikan bahan evaluasi bahkan untuk blog akan ditampilkan kepada pengguna. Website ini dapat berkembang melalui dukungan masyarakat. Sehingga diharapkan dapat memberikan kesadaran masyarakat bahwa sampah memiliki nilai serta menjaga lingkungan tetap bersih. Selain itu diharapkan dapat meningkatkan produktivitas pengepul sampah sehingga efisisensi tenaga dan waktu dalam mengumpulkan sampah.
+        </p>
+      </div>
+      
+      <div class="col-sm-4 col-md-4 col-lg-4">
+          <img src="../../assets/img/about-us.png" alt="Ada Lovelace Portrait" class="img-responsive center-block img-circle" id="adaPortrait">
+        </a>
+      </div>
+    </div>
+    </div>
 
-          <div className='row responsive-about how-to-use-wrapper hero-about d-flex pt-3 pb-3 px-md-0'>
-            <div className='col-6'>
-              <img src="../../assets/img/how-to-use.png" alt="Step by step Illustrator" class="rounded mx-auto d-block" width='50%' /><br><br>
-            </div>
-            <div className='col-6 how-to-use-head'>
-              <h1 class="fw-bold text-center" style="color: #012970;">Cara Penggunaan</h1>
-              <p className='desc-highlight' align="justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper neque ut sodales ornare. Pellentesque vel nisi nulla. Proin ultricies nisl nec tristique ultricies. Quisque volutpat diam et mattis auctor. Curabitur ac neque felis. Proin sed rutrum felis. Vestibulum hendrerit purus at odio facilisis, eu commodo nibh varius. Proin dapibus vulputate metus, at luctus orci pellentesque at. Mauris et sollicitudin massa.</p><br><br>
-            </div>
-          </div>
-    </main>
+    <div class="container-fluid body">
+    <div class="row container-fluid" id="section1">
+      <div class="col-sm-4 col-md-4 col-lg-4">
+        <img src="../../assets/img/how-to-use.png" alt="Ada Lovelace Portrait" class="img-responsive center-block img-circle" id="adaPortrait">
+        </a>
+      </div>
+      
+      <div class="col-sm-8 col-md-8 col-lg-8 textbox">
+        <p class="text-center" id="title">Cara Penggunaan</p>
+        <br>
+        <p class="text-justify text-center" id="bio">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper neque ut sodales ornare. Pellentesque vel nisi nulla. Proin ultricies nisl nec tristique ultricies. Quisque volutpat diam et mattis auctor. Curabitur ac neque felis. Proin sed rutrum felis. Vestibulum hendrerit purus at odio facilisis, eu commodo nibh varius. Proin dapibus vulputate metus, at luctus orci pellentesque at. Mauris et sollicitudin massa.
+        </p>
+      </div>
+    </div>
+    </div>
 
     <section class="bg-white shadow" id="about-us">
       <div class="container" style="padding: 7rem 0;">
