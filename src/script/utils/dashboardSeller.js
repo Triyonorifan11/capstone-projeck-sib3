b96e3388b0934633f14dacf75db4b136898a87fb
Triyono Rifan <triyonorifan01@gmail.com>
2022-11-28 16:00:48 +0700

    add fungsi tambah&kurang pendapatan&pembeli

--- src/script/utils/dashboardSeller.js
+++ src/script/utils/dashboardSeller.js
@@ -36,6 +36,38 @@ const DataDashboardSeller = {
     const jumlProduk = docSnap.data().jumlah_barang;
     await updateDoc(q, { jumlah_barang: jumlProduk - 1 });
   },
+
+  async tambahTotalPendapatan() {
+    const idSeller = getUserInfo().id;
+    const q = doc(db, 'dashboardSellers', idSeller);
+    const docSnap = await getDoc(q);
+    const jumlProduk = docSnap.data().jumlah_pendapatan;
+    await updateDoc(q, { jumlah_pendapatan: jumlProduk + 1 });
+  },
+
+  async kurangTotalPendapatan() {
+    const idSeller = getUserInfo().id;
+    const q = doc(db, 'dashboardSellers', idSeller);
+    const docSnap = await getDoc(q);
+    const jumlProduk = docSnap.data().jumlah_pendapatan;
+    await updateDoc(q, { jumlah_pendapatan: jumlProduk - 1 });
+  },
+
+  async tambahTotalPembeli() {
+    const idSeller = getUserInfo().id;
+    const q = doc(db, 'dashboardSellers', idSeller);
+    const docSnap = await getDoc(q);
+    const jumlProduk = docSnap.data().jumlah_pembeli;
+    await updateDoc(q, { jumlah_pembeli: jumlProduk + 1 });
+  },
+
+  async kurangTotalPembeli() {
+    const idSeller = getUserInfo().id;
+    const q = doc(db, 'dashboardSellers', idSeller);
+    const docSnap = await getDoc(q);
+    const jumlProduk = docSnap.data().jumlah_pembeli;
+    await updateDoc(q, { jumlah_pembeli: jumlProduk - 1 });
+  },
 };
 
 export default DataDashboardSeller;
