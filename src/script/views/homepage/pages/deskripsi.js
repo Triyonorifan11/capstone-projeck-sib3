63dcf38814433f2109e9449606dc59b58b934c20
Triyono Rifan <triyonorifan01@gmail.com>
2022-12-02 16:46:06 +0700

    update tampilan deskripsi produk

--- src/script/views/homepage/pages/deskripsi.js
+++ src/script/views/homepage/pages/deskripsi.js
@@ -1,30 +1,41 @@
 import createAllProducts from '../templates/displayProducts';
+import UrlParser from '../../../routes/url-parser';
 
 const Deskripsi = {
   async render() {
     return `
-      <section class="deskripsi my-5 pt-5">
+      <section class="container deskripsi my-5 mt-3">
         <div class="row mt-5">
-            <div class="col-2 col-md-12 col-12">
-                <img class="img-fluid w-100 h-100" src="." />
+            <div class="col-md-5 p-5">
+                <div class="d-flex justify-content-center align-items-center">
+                  <img class="img-fluid" src="./assets/img/jualbeli.png" width="80%"/>
+                </div>
             </div>
 
-            <div class="col-lg-6 col-md-12 col-12">
-                <h6>Home / Sampah</h6>
-                <h3 class="tittle py-4">Sampah Botol Plastik</h3>
+            <div class="col-md-7">
+                <h3 class="tittle py-4 fw-bold">Sampah Botol Plastik</h3>
                 <h2>Rp 15.000</h2>
-                <input type="number" value="1" />
-                <button class="buy-btn">Add to Cart</button>\
                 <h4 class="mt-4 mb-2">Deskripsi Produk</h4>
-                <span>Sampah botol plastik merupakan bahan padat buangan dari kegiatan manusia yang sudah terpakai. Sampah merupakan konsekuensi dari adanya aktivitas manusia yang begitu kompleks dari mulai bangun tidur hingga tidur lagi, manusia pasti
+                <p>Sampah botol plastik merupakan bahan padat buangan dari kegiatan manusia yang sudah terpakai. Sampah merupakan konsekuensi dari adanya aktivitas manusia yang begitu kompleks dari mulai bangun tidur hingga tidur lagi, manusia pasti
                 menghasilkan buangan atau sampah. Oleh karena itu pengelolaan sampah tidak terlepas dari gaya hidup masyarakat. Jika sampah tersebut terus dibiarkan, tentu akan menimbulkan dampak serius bagi lingkungan yang mengakibatkan pencemaran
-                udara,tanah dan dapat menyebabkan banjir.</span>
+                udara,tanah dan dapat menyebabkan banjir.</p>
+                
+                <div class="btn-group my-3" role="group" aria-label="Basic example">
+                  <button type="button" class="btn btn-sm btn-primary"><i class="bi bi-dash-circle"></i></button>
+                  <input type="number" class="form-control mx-2" id="exampleFormControlInput1" placeholder="1">
+                  <button type="button" class="btn btn-sm btn-primary"><i class="bi bi-plus-circle"></i></button>
+                </div>
+                <div class="d-grid gap-2">
+                  <button class="btn btn-primary" title="checkout" type="button"><i class="bi bi-cart-plus fs-5"></i></button>
+                </div>
             </div>
         </div>
     </section>`;
   },
 
   async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    console.log(url.id);
     console.log('after render deskripsi');
   },
 };
