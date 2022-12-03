b1103e7e5bf0fad593228692a1f91c1016a0894d
luthfikamalananda <luthfi_kamal_ananda@yahoo.com>
2022-12-04 06:32:17 +0700

    checkout can be make and reducing product's stock

---
+++ src/script/utils/addCheckouts.js
@@ -0,0 +1,68 @@
+/* eslint-disable prefer-destructuring */
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore, doc, setDoc,
+} from 'firebase/firestore';
+import { customAlphabet } from 'nanoid';
+import firebaseConfig from '../global/firebase-config';
+import dataProduct from './dataProducts';
+import editTransaction from './editTransactions';
+import flassMessage from './flassMessage';
+import {
+  getUserInfo, escapeHtml,
+} from './functions';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const addCheckout = {
+  async init() {
+    await this.addToDatabase();
+  },
+
+  async addToDatabase(idProduk, idSeller) {
+    const totalPesan = document.querySelector('#totalBeli');
+    const btnSubmit = document.getElementById('btnCheckout');
+    const formAddCheckout = document.getElementById('addCheckout');
+    const dataProduk = await dataProduct._fetchDataProductByIdProduk(idProduk);
+    const currentStock = dataProduk.stok;
+
+    formAddCheckout.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      btnSubmit.innerText = 'Mohon tunggu ...';
+      btnSubmit.classList.add('disabled');
+
+      const calculatePrice = Math.floor(dataProduk.harga) * Math.floor(totalPesan.value);
+      const dataCheckout = {
+        id_barang: escapeHtml(idProduk),
+        id_buyer: getUserInfo().id,
+        id_seller: escapeHtml(idSeller),
+        status: 'diminta',
+        total_beli: escapeHtml(totalPesan.value),
+        total_harga: calculatePrice,
+      };
+
+      const calculateNewStock = Math.floor(currentStock) - Math.floor(totalPesan.value);
+      const updatedStockData = {
+        stok: calculateNewStock,
+      };
+      const nanoid = customAlphabet('1234567890abcdef', 17);
+      const idCheckout = `checkout_${nanoid()}`;
+      await this._insertData(dataCheckout, idCheckout); // add to database checkout
+      await editTransaction._updateStokProduct(updatedStockData, idProduk); // update stok produk
+    });
+  },
+
+  async _insertData(data, id) {
+    try {
+      await setDoc(doc(db, 'checkouts', id), data);
+      flassMessage('success', 'Berhasil!', 'Pemesanan akan diproses!');
+      setTimeout(() => {
+        location.reload();
+      }, 2000);
+    } catch (error) {
+      flassMessage('error', 'Gagal Memesan', `Error= ${error}`);
+    }
+  },
+};
+
+export default addCheckout;
--- src/script/views/homepage/pages/deskripsi.js
+++ src/script/views/homepage/pages/deskripsi.js
@@ -1,63 +1,77 @@
 import UrlParser from '../../../routes/url-parser';
+import addCheckout from '../../../utils/addCheckouts';
+import dataAccount from '../../../utils/dataAccounts';
+import dataProduct from '../../../utils/dataProducts';
+import { formatRupiah } from '../../../utils/functions';
 
 const Deskripsi = {
   async render() {
     return `
-      <section class="container deskripsi my-5 mt-3">
-        <div class="row mt-5">
-            <div class="col-md-5 p-5">
-                <div class="d-flex justify-content-center align-items-center">
-                  <img class="img-fluid" src="./assets/img/jualbeli.png" width="80%"/>
-                </div>
-            </div>
-
-            <div class="col-md-7">
-                <h3 class="tittle py-4 fw-bold">Sampah Botol Plastik</h3>
-                <h2>Rp 15.000</h2>
-                <h4 class="mt-4 mb-2">Deskripsi Produk</h4>
-                <p>Sampah botol plastik merupakan bahan padat buangan dari kegiatan manusia yang sudah terpakai. Sampah merupakan konsekuensi dari adanya aktivitas manusia yang begitu kompleks dari mulai bangun tidur hingga tidur lagi, manusia pasti
-                menghasilkan buangan atau sampah. Oleh karena itu pengelolaan sampah tidak terlepas dari gaya hidup masyarakat. Jika sampah tersebut terus dibiarkan, tentu akan menimbulkan dampak serius bagi lingkungan yang mengakibatkan pencemaran
-                udara,tanah dan dapat menyebabkan banjir.</p>
-                
-                <div class="btn-group my-3" role="group" aria-label="Basic example">
-                  <button type="button" id="btnMin" class="btn btn-sm btn-primary"><i class="bi bi-dash-circle"></i></button>
-                  <input type="number" class="form-control mx-2" id="totalBeli" value="0">
-                  <button type="button" id="btnPlus" class="btn btn-sm btn-primary"><i class="bi bi-plus-circle"></i></button>
-                </div>
-                <div class="d-grid gap-2">
-                  <button class="btn btn-primary" id="btnCheckout" title="checkout" type="button"><i class="bi bi-cart-plus fs-5"></i></button>
-                </div>
-            </div>
-        </div>
+      <section class="container deskripsi my-5 mt-3" id="container">
+        
     </section>`;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
-    const btnPlus = document.querySelector('#btnPlus');
-    const btnMin = document.querySelector('#btnMin');
-    const btnCheckout = document.getElementById('btnCheckout');
-    let totalBeli = parseInt(document.querySelector('#totalBeli').value, 10);
-    btnPlus.addEventListener('click', (e) => {
-      e.preventDefault();
-      totalBeli = isNaN(totalBeli) ? 0 : totalBeli;
-      totalBeli += 1;
-      document.querySelector('#totalBeli').value = totalBeli;
-    });
+    const container = document.getElementById('container');
+    const dataProduk = await dataProduct._fetchDataProductByIdProduk(url.id);
+    const dataSeller = await dataAccount._fetchUserNameById(dataProduk.id_user);
+    container.innerHTML += `
+          <div class="row mt-5">
+            <div class="col-md-5 p-5">
+                <div class="d-flex justify-content-center align-items-center">
+                  <img class="img-fluid" src="${dataProduk.foto}" width="80%"/>
+                </div>
+            </div>
 
-    btnMin.addEventListener('click', (e) => {
-      e.preventDefault();
-      totalBeli = isNaN(totalBeli) ? 0 : totalBeli;
-      totalBeli -= 1;
-      document.querySelector('#totalBeli').value = totalBeli;
-    });
+            <div class="col-md-7">
+                <h3 class="tittle py-4 fw-bold">${dataProduk.nama_product}</h3>
+                <h2>Rp ${formatRupiah(dataProduk.harga)}</h2>
+                <h2>${dataSeller.namalengkap}</h2>
+                <h2>${dataProduk.berat_product} Kg</h2>
+                <h2 id='stokProduk' value='${dataProduk.stok}'>${dataProduk.stok}</h2>
+                <h4 class="mt-4 mb-2">Deskripsi Produk</h4>
+                <p>${dataProduk.deskripsi}</p>
+                
+                <form action="#" method="post" id="addCheckout" enctype="multipart/form-data">
+                  <div class="btn-group my-3" role="group" aria-label="Basic example">
+                    <button type="button" id="btnMin" class="btn btn-sm btn-primary"><i class="bi bi-dash-circle"></i></button>
+                    <input type="number" class="form-control mx-2" id="totalBeli" min='1' max='${dataProduk.stok}'>
+                    <button type="button" id="btnPlus" class="btn btn-sm btn-primary"><i class="bi bi-plus-circle"></i></button>
+                  </div>
+                  <div class="d-grid gap-2">
+                    <button class="btn btn-primary" id="btnCheckout" title="checkout" type="submit"><i class="bi bi-cart-plus fs-5"></i></button>
+                  </div>
+                </form>
+            </div>
+        </div>`;
+    // const btnPlus = document.querySelector('#btnPlus');
+    // const btnMin = document.querySelector('#btnMin');
+    // const btnCheckout = document.getElementById('btnCheckout');
+    await addCheckout.addToDatabase(url.id, dataProduk.id_user);
+    // let totalBeli = parseInt(document.querySelector('#totalBeli').value, 10);
+    // btnPlus.addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   totalBeli = isNaN(totalBeli) ? 0 : totalBeli;
+    //   totalBeli += 1;
+    //   document.querySelector('#totalBeli').value = totalBeli;
+    // });
 
-    btnCheckout.addEventListener('click', (e) => {
-      e.preventDefault();
-      console.log(totalBeli);
-    });
+    // btnMin.addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   totalBeli = isNaN(totalBeli) ? 0 : totalBeli;
+    //   totalBeli -= 1;
+    //   document.querySelector('#totalBeli').value = totalBeli;
+    // });
 
-    console.log(url.id);
+    // btnCheckout.addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   console.log(totalBeli);
+    //   console.log(document.querySelector('#totalBeli').value);
+    // });
+
+    // console.log(url.id);
   },
 };
 
