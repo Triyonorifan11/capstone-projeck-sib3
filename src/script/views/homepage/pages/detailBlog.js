e912578659967463c61f68d5c708773a156bf050
Hesti Yuana Mahardika <hestiyuanamahardika@gmail.com>
2022-12-04 23:14:09 +0700

    Update detail blog

--- src/script/views/homepage/pages/detailBlog.js
+++ src/script/views/homepage/pages/detailBlog.js
@@ -1,48 +1,47 @@
 // import Blog from './blog';
 import UrlParser from '../../../routes/url-parser';
+import addBlogs from '../../../utils/addBlogs';
 import dataPosts from '../../../utils/dataBlogs';
 import { formatDate } from '../../../utils/functions';
 
 const DetailBlog = {
   async render() {
     return `
-    <div class="blog-detail">
-        <div id="data" class="data"></div>
-    </div>
-    `;
+      <section class="container deskripsi my-5 mt-3" id="container">
+        
+    </section>`;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
-    const fetchedBlog = await dataPosts._fetchAllDataPosts();
-    const postsDetail = document.getElementById('postsDetail');
+    const container = document.getElementById('container');
+    const dataPost = await dataPosts._fetchDataPostsByIdPost(url.id);
+    // const fetchedBlog = await dataPosts._fetchAllDataPosts();
+    // const postsDetail = document.getElementById('postsDetail');
     // const blogContainer = document.querySelector('#data');
     // blogContainer.innerHTML = Blog(data);
-    fetchedBlog.forEach((d) => {
-      const data = d.data();
-      data.id = d.id;
+    // fetchedBlog.forEach((d) => {
+    //   const data = d.data();
+    //   data.id = d.id;
 
-      postsDetail.innerHTML += `
-        <div class="content">
-            <div class="bg-section">
-            <h2 align="center">${data.judul}</h2>
-        </div>
-        <img class="blog__poster" src="${data.foto}" alt="${data.judul}" />
-        <div class="blog__info">
-            <h3>Information</h3>
-            <h4><i class="fa-solid fa-medal"></i>&nbsp; Tanggal</h4>
-            <p> ⭐️ ${formatDate(data.tgl_dibuat)}</p><br>
-            <h4><i class="fa-solid fa-magnifying-glass"></i>&nbsp; Categories</h4>
-            <p>${data.kategori}</p><br>
-        </div><br><br><br>
-        <div class="blog__overview">
-            <h3>Description</h3>
-            <p>${data.deskripsi}</p>
-        </div>
+    container.innerHTML += `
+    <div class="row mt-5">
+      <div class="col-md-5 p-5">
+          <div class="d-flex justify-content-center align-items-center">
+            <img class="img-fluid" src="${dataPost.foto}" width="80%"/>
+          </div>
+      </div>
+
+      <div class="col-md-7">
+          <h3 class="tittle py-4 fw-bold">${dataPost.judul}</h3>
+          <h2>${formatDate(dataPost.tgl_dibuat)}</h2>
+          <h2>${dataPost.kategori}</h2>
+          <h4 class="mt-4 mb-2">Deskripsi Produk</h4>
+          <p>${dataPost.deskripsi}</p>
+      </div>
+    </div>
         `;
-    });
-
-    console.log(url.id);
+    await addBlogs.addToDatabase(url.id, dataPost);
   },
 };
 
